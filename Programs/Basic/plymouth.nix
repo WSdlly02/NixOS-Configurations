0fad12ec26e4e36cb14e4aaaae68abbf2ecffc33
wsdlly02 <wsdlly02@hotmail.com>
2024-02-02 01:47:23 +0800

    second commitment 2024-02-02

--- Programs/Basic/plymouth.nix
+++ Programs/Basic/plymouth.nix
@@ -8,4 +8,14 @@
     ];
     theme = "rings";
   };
+  systemd.services.plymouth-wait-for-animation = {
+    enable = true;
+    before = ["plymouth-quit.service" "display-manager.service"];
+    wantedBy = ["plymouth-start.service"];
+    unitConfig = { Description = "Waits for Plymouth animation to finish"; };
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.coreutils}/bin/sleep 6";
+    };
+  };
 }
