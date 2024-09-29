912799d1caefd47ba8d300856564aa9b72406f0b
wsdlly02 <wsdlly02@hotmail.com>
2024-09-29 17:47:18 +0800

    2024-09-29

--- WSdlly02-PC-CONF/Hardware/remotefsmount.nix
+++ WSdlly02-PC-CONF/Hardware/remotefsmount.nix
@@ -2,9 +2,7 @@
   fileSystems."/home/wsdlly02/Disks/Lily-PC" = 
     { device = "lily@lily-pc.local:/";
       fsType = "fuse.sshfs";
-      options = [ "x-systemd.automount" "_netdev" "users" "idmap=user" 
-      "IdentityFile=/home/wsdlly02/.ssh/id_rsa_lily"
-      "allow_other" "reconnect" "ServerAliveInterval=15" "port=10022"
+      options = [ "x-systemd.automount" "noauto" "nofail" "_netdev" "users" "idmap=user" "IdentityFile=/home/wsdlly02/.ssh/id_rsa_lily" "allow_other" "reconnect" "ServerAliveInterval=15" "port=10022"
       ];
     };
 }
