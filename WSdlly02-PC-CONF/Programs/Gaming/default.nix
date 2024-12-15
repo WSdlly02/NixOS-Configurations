2a4435c9e95ba285eeb70297a48565c209977507
wsdlly02 <wsdlly02@hotmail.com>
2024-12-15 17:10:26 +0800

    2024-12-15

--- WSdlly02-PC-CONF/Programs/Gaming/default.nix
+++ WSdlly02-PC-CONF/Programs/Gaming/default.nix
@@ -22,7 +22,7 @@
     };
     java = {
       enable = true;
-      package = pkgs.zulu;
+      package = pkgs.zulu21;
     };
   };
   environment.defaultPackages = with pkgs; [
@@ -33,5 +33,16 @@
     mindustry
     prismlauncher
     mcrcon
+  ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      mindustry = prev.mindustry.override {jdk17 = pkgs.zulu17;};
+      prismlauncher = prev.prismlauncher.override {
+        jdk21 = pkgs.zulu21; # Actually, it's useless, because Prismlauncher doesn't really need java.
+        jdk17 = null; # Strip
+        jdk8 = null; # Strip
+        jdks = [pkgs.zulu21]; # Add JAVA_PATH for Prismlauncher
+      };
+    })
   ];
 }
