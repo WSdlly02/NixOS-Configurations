90c4cf3c8173fa4521bdcca2d4e149c015814b97
wsdlly02 <wsdlly02@hotmail.com>
2025-01-10 17:54:38 +0800

    2025-01-10
    update flake.lock

--- WSdlly02-PC/Programs/Basic/system.nix
+++ WSdlly02-PC/Programs/Basic/system.nix
@@ -15,7 +15,9 @@
     };
   };
   nixpkgs = {
-    hostPlatform = lib.mkDefault "x86_64-linux";
+    hostPlatform = lib.mkDefault "x86_64-linux"; # specific this option blocks nixpkgs.crossSystem
+    # localSystem = null; # equals to nixpkgs.buildPlatform
+    # buildPlatform = config.nixpkgs.hostPlatform;
     config = {
       allowUnfree = true;
       allowUnsupportedSystem = true;
@@ -23,7 +25,18 @@
       rocmSupport = true;
     };
   };
+  /*
+  nixpkgs.crossSystem = {
+    # Target platform
+    system = "riscv64-linux";
+  };
+  Specifies the platform for which NixOS should be built.
+  Specify this only if it is different from nixpkgs.localSystem,
+  the platform on which NixOS should be built.
+  In other words, specify this to cross-compile NixOS.
+  */
   boot.binfmt.emulatedSystems = [
+    # use QEMU to emulate systems for compiling pkgs of different archs.
     "x86_64-windows"
     "aarch64-linux"
   ];
