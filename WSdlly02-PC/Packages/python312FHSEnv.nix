9486f9495ab2268426839e8a066ddec2b6c23e7c
wsdlly02 <wsdlly02@hotmail.com>
2025-01-12 19:07:19 +0800

    2025-01-12
    Python Update

--- WSdlly02-PC/Packages/python312FHSEnv.nix
+++ WSdlly02-PC/Packages/python312FHSEnv.nix
@@ -1,5 +1,35 @@
+/*
+Notice:
+
+python3 = python312; # which is defined in <nixpkgs/pkgs/top-level/all-packages.nix>
+python3Packages = dontRecurseIntoAttrs python312Packages; # same as above
+python312Packages = recurseIntoAttrs python312.pkgs; # same as above
+some packages requires old python-modules will specific {python3 = python311;} in args
+
+buildEnv = callPackage ./wrapper.nix {
+  python = self;
+  inherit (pythonPackages) requiredPythonModules;
+};
+withPackages = import ./with-packages.nix { inherit buildEnv pythonPackages; }; which is defined in <nixpkgs/pkgs/development/interpreters/python/passthrufun.nix>
+pkgs = pythonPackages; # same as above
+
+virtualenv = with python3Packages; toPythonApplication virtualenv; # virtualenv = python3Packages.virtualenv
+"toPythonApplication" converts a Python library to an application, which is defined in <nixpkgs/pkgs/development/interpreters/python/python-packages-base.nix>
+
+python3.buildEnv.override {
+  extraLibs = [ python3Packages.pyramid ];
+  ignoreCollisions = true;
+}
+
+is equals to
+
+python3.withPackages (ps: with ps; [
+  numpy
+  requests
+])
+*/
 {
-  pkgs,
+  buildFHSEnv,
   cmake,
   gcc,
   glibc,
@@ -9,15 +39,17 @@
   ninja,
   ncurses,
   udev,
-  uv,
-  virtualenv,
-  python312,
   zstd,
-  buildFHSEnv,
+  python312,
+  # numpy, # which is used in buildPythonPackages {propagatedBuildInputs=[];}
 }: let
-  usedPython312Packages = [
-    virtualenv
-  ];
+  usedPython312Packages = python312.withPackages (ps:
+    with ps; [
+      numpy
+      pandas
+      uv
+      virtualenv
+    ]);
   /*
   usedRocmPackages = [
     clr
@@ -37,23 +69,20 @@ in
   buildFHSEnv {
     name = "python312FHSEnv";
     targetPkgs = (
-      pkgs:
-        [
-          # Common pkgs
-          cmake
-          gcc
-          glibc
-          dbus
-          fish
-          libdrm
-          ninja
-          ncurses
-          udev
-          uv
-          python312
-          zstd
-        ]
-        ++ usedPython312Packages
+      pkgs: [
+        # Common pkgs
+        cmake
+        gcc
+        glibc
+        dbus
+        fish
+        libdrm
+        ninja
+        ncurses
+        udev
+        zstd
+        usedPython312Packages
+      ]
       # ++ usedRocmPackages
     );
     runScript = "fish";
