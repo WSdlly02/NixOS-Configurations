86cc1a6019a9ccbe660c7de665579d39fded7bf2
wsdlly02 <wsdlly02@hotmail.com>
2025-01-17 23:50:29 +0800

    ...

--- modules/Infrastructure/bluetooth.nix
+++ modules/Infrastructure/bluetooth.nix
@@ -5,10 +5,11 @@
 }: {
   hardware.bluetooth = {
     enable = lib.mkIf (config.system.name == "WSdlly02-PC" || config.system.name == "WSdlly02-RaspberryPi5") true;
+    powerOnBoot = lib.mkIf (config.system.name == "WSdlly02-RaspberryPi5") false;
     # hsphfpd.enable = true; Conflicts with wireplumber
     settings = {
       General = {
-        Enable = lib.mkIf (config.system.name == "WSdlly02-PC") "Source,Sink,Media,Socket";
+        Enable = "Source,Sink,Media,Socket";
         FastConnectable = true;
         Experimental = true;
       };
