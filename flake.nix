160f9a5df49f75ea38545e98c90f3d58a4fa758a
wsdlly02 <wsdlly02@hotmail.com>
2025-01-30 13:30:26 +0800

    ...

--- flake.nix
+++ flake.nix
@@ -1,18 +1,6 @@
 {
   description = "WSdlly02's NixOS flake";
 
-  nixConfig = {
-    extra-substituters = [
-      # nix community's cache server
-      "https://nix-community.cachix.org"
-    ];
-    # will be appended to the system-level trusted-public-keys
-    extra-trusted-public-keys = [
-      # nix community's cache server public key
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
