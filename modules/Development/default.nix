6aac5262e163459e65f88e03a6b9df6ab36a9b34
wsdlly02 <wsdlly02@hotmail.com>
2025-02-06 02:30:53 +0800

    rust toolchain update

--- modules/Development/default.nix
+++ modules/Development/default.nix
@@ -1,16 +1,28 @@
+/*
+  pkgs.rust.packages.stable.rustc = pkgs.rustc
+  pkgs.rustPlatform.buildRustPackage = pkgs.rust.packages.stable.rustPlatform.buildRustPackage
+*/
 {
   inputs,
   pkgs,
   ...
 }:
 {
-  environment.systemPackages = with pkgs; [
-    # Rust toolchain
-    cargo
-    rustc
-    rustfmt
-    inputs.my-codes.packages."${pkgs.system}".python312Env
-    # inputs.my-codes.packages."${pkgs.system}".python312FHSEnv
-    # Other pkgs
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      gcc
+      # Rust toolchains
+      cargo
+      clippy
+      rustc
+      rustfmt
+      # Python 3.12
+      inputs.my-codes.packages."${pkgs.system}".python312Env
+      # inputs.my-codes.packages."${pkgs.system}".python312FHSEnv
+      # Other pkgs
+    ];
+    variables = {
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
+  };
 }
