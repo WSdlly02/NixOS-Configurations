38534a6a93ebbbe7209fef7ed9ac244aad3f14bf
wsdlly02 <wsdlly02@hotmail.com>
2025-03-12 10:09:24 +0800

    ...

--- modules/Infrastructure/nix.nix
+++ modules/Infrastructure/nix.nix
@@ -10,7 +10,6 @@
       substituters = lib.mkForce [
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-        "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://cache.nixos.org"
       ];
       trusted-users = [
